d4c2a1733094e4396bf877bbd106340b70eec138
Francis Lussier <daesolus6@gmail.com>
2018-04-01 13:42:32 -0400

    Add config for test

--- api/config.py
+++ api/config.py
@@ -5,9 +5,7 @@ import os
 class Config:
     def __init__(self):
         parser = ConfigParser()
-        script_dir = os.path.dirname(__file__)
-        abs_file_path = os.path.join(script_dir, 'config.ini')
-        found = parser.read(abs_file_path)
+        found = parser.read('config.ini')
         if not found:
             raise ValueError('No config file found!')
         for name in parser.sections():
--- test/test_config.py
+++ test/test_config.py
@@ -5,8 +5,14 @@ from api.config import config
 
 class TestConfig(unittest.TestCase):
 
-    def test_get_api_key_success(self):
-        self.assertEqual('404', config.get('api_key'))
+    def test_get_dialogflow_secret_success(self):
+        self.assertEqual('file.json', config.get('dialogflow_secret'))
+
+    def test_get_project_id_success(self):
+        self.assertEqual('whisperproject', config.get('project_id'))
+
+    def test_get_language_success(self):
+        self.assertEqual('en-US', config.get('language'))
 
     @raises(AttributeError)
     def test_invalid_config_key(self):
