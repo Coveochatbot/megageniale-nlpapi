728458aa86441e712343fe66d9e2ef74c726c835
Francis Lussier <daesolus6@gmail.com>
2018-04-08 15:35:42 -0400

    Entities detection

--- api/dialogflow_service.py
+++ api/dialogflow_service.py
@@ -2,7 +2,9 @@ import os
 import uuid
 import dialogflow
 from model.intent import Intent
+from model.entity import Entity
 from config import config
+from model.NLPAnalysis import NLPAnalysis
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = config.get('dialogflow_secret')
 
 
@@ -12,10 +14,26 @@ class DialogFlowService:
         self.project_id = config.get('project_id')
         self.parent = 'projects/{}/agent'.format(self.project_id)
 
-    def detect_intent(self, text):
+    def analyse_sentence(self, sentence):
+        nlp_analysis = NLPAnalysis()
+        query_result = self.detect_intent(sentence)
+        nlp_analysis.add_intent(self.retrieve_intent(query_result))
+        self.retrieve_entities(nlp_analysis, query_result)
+        return nlp_analysis
+
+    def detect_intent(self, sentence):
         session_client = dialogflow.SessionsClient()
         session = session_client.session_path(self.project_id, self.session_id)
-        text_input = dialogflow.types.TextInput(text=text, language_code=config.get('language'))
+        text_input = dialogflow.types.TextInput(text=sentence, language_code=config.get('language'))
         query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
-        return Intent(response.query_result.intent.display_name, response.query_result.intent_detection_confidence)
+        return response.query_result
+
+    def retrieve_intent(self, query_result):
+        return Intent(query_result.intent.display_name, query_result.intent_detection_confidence)
+
+    def retrieve_entities(self, nlp_analysis, query_result):
+        for i, entity in query_result.parameters.items():
+            nlp_analysis.add_entity(Entity(entity))
+
+
--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
-from flask import Flask,jsonify, json, request
-from model.intent import Intent
-from model.entity import Entity
-from model.NLPAnalysis import NLPAnalysis
+from flask import Flask,jsonify, request
 from NLPJSONEncoder import NLPJSONEncoder
 from dialogflow_service import DialogFlowService
 app = Flask(__name__)
@@ -12,11 +9,7 @@ dialogflowservice = DialogFlowService()
 @app.route('/NLP/Analyse',  methods=['POST'])
 def hello_world():
     content = request.get_json()
-    nlp_analysis = NLPAnalysis()
-    intent = dialogflowservice.detect_intent(content['sentence'])
-    entity = Entity("test")
-    nlp_analysis.add_entity(entity)
-    nlp_analysis.add_intent(intent)
+    nlp_analysis = dialogflowservice.analyse_sentence(content['sentence'])
     return jsonify(nlp_analysis)
 
 
