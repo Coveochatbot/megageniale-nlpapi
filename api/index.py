58725d47420e47b0a4ba887ffd8912b6603addb8
Christopher <clemonde28@gmail.com>
2018-04-13 15:36:09 -0400

    Added logger functionality

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask,jsonify, request
 from api.NLPJSONEncoder import NLPJSONEncoder
 from api.dialogflow_service import DialogFlowService
+from api.logger.logger_factory import LoggerFactory
 app = Flask(__name__)
 app.json_encoder = NLPJSONEncoder
 dialogflowservice = DialogFlowService()
@@ -14,4 +15,5 @@ def nlp_analyze():
 
 
 if __name__ == '__main__':
+    LoggerFactory.get_logger(__name__).info("API started")
     app.run(host='0.0.0.0')
